b8392aeec5421c75f6ed698e7499ac1e3a19e5bb
Gurdip Sira <87333788+GurdipS5@users.noreply.github.com>
2026-01-07 01:38:10 +0000

    Update data.tf

--- src/data.tf
+++ src/data.tf
@@ -1,10 +1,3 @@
 data "octopusdeploy_project_groups" "groups" {
   space_id = var.space_id
 }
-
-locals {
-  project_group_id = one([
-    for g in data.octopusdeploy_project_groups.groups.project_groups :
-    g.id if g.name == var.project_group_name
-  ])
-}
