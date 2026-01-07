e269fdd06da64385eb6d332f3a363d012ca821b6
Gurdip Sira <87333788+GurdipS5@users.noreply.github.com>
2026-01-07 01:37:46 +0000

    Update locals.tf

--- src/locals.tf
+++ src/locals.tf
@@ -1,4 +1,9 @@
 locals {
   effective_lifecycle_name      = coalesce(var.lifecycle_name, "${var.project_name} Lifecycle")
   effective_library_varset_name = coalesce(var.library_variable_set_name, "${var.project_name} Library Variables")
+
+  project_group_id = one([
+    for g in data.octopusdeploy_project_groups.groups.project_groups :
+    g.id if g.name == var.project_group_name
+  ])
 }
