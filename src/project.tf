6b31f5c765f2d940ab995a39ee8360ad9721ac0f
Gurdip Sira <87333788+GurdipS5@users.noreply.github.com>
2026-01-07 01:33:01 +0000

    Update project.tf

--- src/project.tf
+++ src/project.tf
@@ -1,17 +1,23 @@
 resource "octopusdeploy_project" "this" {
-  space_id = var.space_id
-  name = var.project_name
-  description = var.project_description
-  is_disabled = var.is_disabled
-  lifecycle_id = local.effective_lifecycle_id
-  project_group_id = local.project_group_id
+  for_each = var.projects
+
+  space_id         = var.space_id
+  name             = each.key
+  description      = try(each.value.description, null)
+  is_disabled      = try(each.value.is_disabled, false)
+  project_group_id = local.project_group_ids[each.key]
+
+  lifecycle_id = try(
+    each.value.lifecycle_id,
+    local.effective_lifecycle_id
+  )
 
   dynamic "git_persistence_settings" {
-    for_each = var.git_persistence_settings == null ? [] : [var.git_persistence_settings]
+    for_each = try(each.value.git, null) == null ? [] : [each.value.git]
     content {
       default_branch = git_persistence_settings.value.default_branch
-      base_path = git_persistence_settings.value.base_path
-      url = git_persistence_settings.value.url
+      base_path      = git_persistence_settings.value.base_path
+      url            = git_persistence_settings.value.url
       credentials_id = git_persistence_settings.value.credentials_id
     }
   }
